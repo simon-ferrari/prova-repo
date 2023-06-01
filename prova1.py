253e026b23f26a1429525ae4ed4567767556bf61
simon-ferrari <134499103+simon-ferrari@users.noreply.github.com>
2023-06-01 11:04:13 +0000

    prova

--- prova1.py
+++ prova1.py
@@ -1,2 +1,13 @@
 # aggiungi commento
 # aggiungi altro commento
+#%%
+
+print("protein lover")
+print("meglio visitare le rovine di machu pichu che ti visiti un macho e ti metta il pichu rovinandoti")
+
+# %%
+
+nome = "simon" 
+
+print(nome)
+# %%
